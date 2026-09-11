5d0ab76b714af2d4024c3ea7b5d14c6a997d4874
devzone-creator <munkailasule3@gmail.com>
2026-09-11 12:30:45 +0000

    Add root Toolforge process configuration

---
+++ toolforge-start.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+set -eu
+
+BACKEND_PORT="${BACKEND_PORT:-8001}"
+
+PORT="$BACKEND_PORT" HOST="127.0.0.1" python server/run.py &
+backend_pid=$!
+
+cleanup() {
+  kill "$backend_pid" 2>/dev/null || true
+}
+
+trap cleanup EXIT INT TERM
+
+export BACKEND_API_URL="${BACKEND_API_URL:-http://127.0.0.1:${BACKEND_PORT}}"
+export HOST="${HOST:-0.0.0.0}"
+exec node client/dist/server/server.js
